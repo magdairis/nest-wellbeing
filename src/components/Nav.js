531c97837db4101680b7d41cff4f23c60f86eb78
magdairis <magdapetford@gmail.com>
2020-05-08 13:17:06 +0100

    removed gatsby fluff

---
+++ src/components/Nav.js
@@ -0,0 +1,36 @@
+import { useLocation } from "@reach/router"
+import { motion } from "framer-motion"
+import React from "react"
+import { Link } from "./Link"
+
+const links = [
+    { href: "/", label: "Home" },
+    // { href: "/blog", label: "Blog" },
+    // { href: "/about", label: "About" },
+    { href: "/contact", label: "Contact" },
+]
+
+const NavLink = ({ children, href, label, ...props }) => {
+    // const { pathname } = useLocation()
+    // const pathBeginning = `/${pathname.split("/")[1]}`
+    // const active = pathBeginning === href
+    return children ? (
+        children({ href, label, active, ...props })
+    ) : (
+            <Link to={href} {...props}>
+                {label}
+            </Link>
+        )
+}
+
+const Nav = ({ children, ...props }) => {
+    return (
+        <nav {...props}>
+            {links.map(linkProps => (
+                <NavLink key={linkProps.href} children={children} {...linkProps} />
+            ))}
+        </nav>
+    )
+}
+
+export default Nav
